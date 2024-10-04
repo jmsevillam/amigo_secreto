2cf7c5d9b21d1b81edde7acd7acad6cb3cf3345a
Mauricio Sevilla <jmasevillam@gmail.com>
2024-10-04 17:36:10 +0200

    Update amigo.js

--- amigo.js
+++ amigo.js
@@ -2,8 +2,9 @@ var a = 123
 var reset = false;
 var array = ["Diego","Mafe","Sharon","Naomi","Francesca","Rodrigo","Maria Micheva", "Mareike","Max","Robin","Nancy","Emilie","Mao", "María Martínez", "Anton"];
 var code = {123 :0, 456 :1, 789 :2,147 :3,258 :4, 369 :5,1230 :6,4560 :7,7890 :8,1470 :9,2580 :10,3690 :11,159 :12,1590 : 13,357 :14};
-var canv = document.getElementById("canv");
-var $ = canv.getContext("2d");
+
+const canv = document.getElementById("canv");
+const $ = canv.getContext("2d");
 
 $.fillText("your secret friend is: "+array[code[a]]);
 function animate(){
@@ -19,6 +20,8 @@ function animate(){
     alert("Test");
 
 
+ctx.font = "20px Georgia";
+ctx.fillText("Hello World!", 10, 50);
     
     reset = false;
   }
