0d867e6202e99783cf95ac306ef07e7295c4dcba
Mauricio Sevilla <jmasevillam@gmail.com>
2024-10-04 16:54:19 +0200

    Update amigo.js

--- amigo.js
+++ amigo.js
@@ -11,6 +11,7 @@ var $ = canv.getContext("2d");
     a = document.getElementById('inputa').value;
     //document.writeln("your secret friend is: "+array[code[a]], 10, 90);
     $.font = "50px Arial";
+    $.fillStyle = "rgb(255,255,255)";
     $.fillText("your secret friend is: "+array[code[a]]);
     
     reset = false;
